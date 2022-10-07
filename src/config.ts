dfe5012ade0ce961ca5b96a3aaf604e41d903c71
David Sabalete Rodríguez <dsabalete@gmail.com>
2022-10-07 22:14:48 +0200

    feat: use icons in the navbar

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Creu Alta Sabadell Bàsquet';
+export const SITE_TITLE = 'Web del Creu Alta Sabadell Bàsquet';
 export const SITE_DESCRIPTION = 'Benvinguts a la web del Creu Alta!';
