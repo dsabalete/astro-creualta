9039e750c2a2698d6f88297a26acff177e36a019
David Sabalete Rodríguez <dsabalete@gmail.com>
2022-10-12 23:55:04 +0200

    feat: add shop link to Sections

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Web del Creu Alta Sabadell Bàsquet';
+export const SITE_TITLE = 'Creu Alta Sabadell Bàsquet';
 export const SITE_DESCRIPTION = 'Aquesta és la web del Club de Bàsquet de la Creu Alta de Sabadell. Aquí podràs trobar informació sobre el club, noticies, dades dels equips i dates i horaris de les competicions.';
